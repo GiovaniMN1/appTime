941e6c474ba1acc76e2baab7f04284e6fd478334
Giovani Madero <gioaldo172@gmail.com>
2023-05-22 04:54:06 -0600

    Nuevos cambios, creacion de componentes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,41 @@
-
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faClock, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import './../elements/HeaderStyles.css'
 
 const Header=()=>{
     return(
-        <header>
-          <h1>Mi blog personal</h1>
+        <ContainerHeader>
+          <TitleHeader>Title Lorem isep</TitleHeader>
           
-          <nav>
-            <NavLink to='/'><FontAwesomeIcon icon={faCoffee } /></NavLink>
-            <NavLink to='/time'>Time</NavLink>
+          <ContainerNav>
+            <NavLink to='/'><FontAwesomeIcon icon={faClock } className="navA"/></NavLink>
+            <NavLink to='/time'><FontAwesomeIcon icon={faPenToSquare} className="navA" /></NavLink>
             
-          </nav>
-        </header>
+          </ContainerNav>
+        </ContainerHeader>
     )
 }
+const ContainerHeader=styled.header`
+  display:flex;
+  width:100%;
+  height:20vh;
+  background-color:#1B1A17;
+  flex-direction:column;
+  
+`
+const ContainerNav=styled.nav`
+  width:30%;
+  display:flex;
+  justify-content:space-evenly;
+`
+const TitleHeader=styled.h1`
+  font-size:1.4rem;
+  font-weight:300;
+  padding-left:30px;
+  text-align:center;
+`
+
 
 export default Header
--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,17 +1,77 @@
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
-const Time=()=>{
-    return(
-        <ContainerTime>
-            <h2>Pagina de Acerca</h2>
-            <p>Edte es un elemento</p>
-        </ContainerTime>
-    )
+
+const Time =()=> {
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+    setElapsedTime(totalSeconds);
+  }
+
+  useEffect(() => {
+    let interval;
+    if (elapsedTime > 0) {
+      interval = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => {
+          if (prevElapsedTime > 0) {
+            return prevElapsedTime - 1;
+          } else {
+            clearInterval(interval);
+            return 0;
+          }
+        });
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [elapsedTime]);
+
+  function formatTime(time) {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
+
+  return (
+    <ContainerTime>
+      <ContainerForm onSubmit={handleSubmit}>
+        <LabelTitle>
+          Horas:
+          <input type="number" value={hours} onChange={(e) => setHours(parseInt(e.target.value, 10))} />
+        </LabelTitle>
+        <label>
+          Minutos:
+          <input type="number" value={minutes} onChange={(e) => setMinutes(parseInt(e.target.value, 10))} />
+        </label>
+        <label>
+          Segundos:
+          <input type="number" value={seconds} onChange={(e) => setSeconds(parseInt(e.target.value, 10))} />
+        </label>
+        <button type="submit">Iniciar</button>
+      </ContainerForm>
+      {elapsedTime > 0 && <div>Tiempo restante: {formatTime(elapsedTime)}</div>}
+    </ContainerTime>
+  );
 }
 const ContainerTime=styled.div`
+    display: flex;
+    height:80vh;
+
+    align-items:center;
+    justify-content:center;
+    
+`
+const ContainerForm=styled.form`
     display:flex;
-    height:60px;
-    background-color:black;
+    flex-direction:column;
+`
+const LabelTitle=styled.label`
+    margin-right:10px;
 `
 
-
-export default Time
+export default Time;
